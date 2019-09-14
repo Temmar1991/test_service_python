44e05d716472d76bb8e845ba87d48da66b2c99b6
unknown <temmar1991@gmail.com>
2019-09-14 22:12:40 +0300

    Redesign connect method

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy
 import requests
 import settings
-import models
 import uvicorn
 
 schemas = SchemaGenerator(
@@ -19,6 +18,8 @@ app = Starlette()
 app.debug = settings.DEBUG
 
 base = declarative_base()
+db_string = settings.DATABASE_URL
+db = create_engine(db_string)
 
 
 class ResultsTwitter(base):
@@ -27,32 +28,27 @@ class ResultsTwitter(base):
 
     id = Column(Integer, primary_key=True)
     tweet_id = Column('tweet_id', BigInteger)
+    hashtag = Column('hashtag', String)
     tweet = Column('tweet', String)
     username = Column('user', String)
     created_at = Column(String)
 
-    def __init__(self, tweet_id, tweet, username, created_at):
+    def __init__(self, tweet_id, hashtag, tweet, username, created_at):
         self.tweet_id = tweet_id
+        self.hashtag = hashtag
         self.tweet = tweet
         self.username = username
         self.created_at = created_at
 
 
-def connect(tweet_id, tweet, username, createdat):
-    db_string = settings.DATABASE_URL
-    db = create_engine(db_string)
+def connect():
     try:
         Session = sessionmaker(bind=db)
         session = Session()
         base.metadata.create_all(db)
-        entry = ResultsTwitter(tweet_id, tweet, username, createdat)
-        session.add(entry)
-        session.commit()
-        session.close()
+        return session
     except sqlalchemy.exc.DatabaseError as e:
         print(e)
-    return
-
 
 @app.route('/tweets', methods=['GET'])
 async def get_last_tweet(request):
@@ -71,27 +67,40 @@ async def get_last_tweet(request):
                       )
         return {'error': errors}
 
-    search_list, id_list, pubtime_list, user_list = [], [], [], []
+    search_list, id_list, pubtime_list, user_list, hashtag_list = [], [], [], [], []
 
     statuses = search_request.json().get('statuses')
 
     for status in statuses:
         if 'text' in status:
-            text_field = status.get('text')
+            tweet = status.get('text')
             id_message = status.get('id')
             published_at = status.get('created_at')
-            user = status.get('user').get('id')
-            connect(id_message, text_field, user, published_at)
-            search_list.append(text_field)
+            user = status.get('user').get('id').get('name')
+            hashtag = status['entities']['hashtags'].setdefault('text', None)
+            hashtag_list.append(hashtag)
+            entry = ResultsTwitter(id_message, hashtag, tweet, user, published_at)
+            ses = connect()
+            ses.add(entry)
+            ses.commit()
+            ses.close()
+            search_list.append(tweet)
             id_list.append(id_message)
             pubtime_list.append(published_at)
             user_list.append(user)
+
     response = {'id': id_list,
                 'phrase': search_list,
                 'published_at': pubtime_list,
-                'author_id': user_list}
+                'author_id': user_list,
+                'hashtags': hashtag_list}
     # return JSONResponse(statuses)
     return JSONResponse(response)
+
+
+@app.route('/uniq', methods=['GET'])
+async def get_uniq_tweet():
+    pass
 
 
 if __name__ == '__main__':
